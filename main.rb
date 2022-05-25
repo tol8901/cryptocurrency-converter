4fb05cb9d084c003a68448fe775cdcf81a5b7f61
Anatolii Kravchenko <anatoliy.kravchenko1991@gmail.com>
2022-05-25 21:11:27 +0300

    implement the menu function

---
+++ main.rb
@@ -0,0 +1,16 @@
+def menu
+
+  loop do
+    puts " Cryptcurrency Converter ".center(50, "#")
+    puts "a) Convert to USD"
+    puts "b) Convert to EUR"
+    puts "q) Quit"
+    puts "Action: "
+    choice = gets.chomp
+
+    break unless choice != 'q'
+  end
+
+end
+
+menu()
